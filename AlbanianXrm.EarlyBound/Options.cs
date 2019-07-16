3a63f7276d85859aed2af01159704b74b3d8bd9e
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2019-07-16 22:55:39 +0200

    Improved configuration. Now you can save configurations per organization.

--- AlbanianXrm.EarlyBound/Options.cs
+++ AlbanianXrm.EarlyBound/Options.cs
@@ -1,11 +1,20 @@
 ﻿using AlbanianXrm.EarlyBound.Helpers;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing.Design;
+using System.Linq;
+using System.Xml.Serialization;
 
 namespace AlbanianXrm.EarlyBound
 {
     public class Options
     {
+        public Options()
+        {
+            OrganizationOptions = new Dictionary<string, OrganizationOptions>();
+        }
+
         [Category("General")]
         [DisplayName("Output")]
         [Description("Output path")]
@@ -13,14 +22,44 @@ namespace AlbanianXrm.EarlyBound
         public string Output { get; set; }
 
         [Category("General")]
-        [DisplayName("Namespace")]
-        [Description("Generated namespace")]
-        public string Namespace { get; set; }
-
-        [Category("General")]
         [DisplayName("Language")]
         [Description("Output file language")]
         [TypeConverter(typeof(DescriptionEnumConverter))]
-        public LanguageEnum Language { get; set; }      
+        public LanguageEnum Language { get; set; }
+
+        [Category("Organization")]
+        [DisplayName("Organization Options")]
+        [Description("Current organization options")]
+        [TypeConverter(typeof(ExpandableObjectConverter))]
+        [XmlIgnore]
+        public OrganizationOptions CurrentOrganizationOptions { get; set; }
+
+        [Browsable(false)]
+        public OrganizationOptions[] OrganizationOptionsList
+        {
+            get => OrganizationOptions.Values.ToArray();
+            set
+            {
+                OrganizationOptions = new Dictionary<string, OrganizationOptions>();
+                if (value != null)
+                {
+                    foreach (var item in value)
+                    {
+                        if (OrganizationOptions.ContainsKey(item.Key))
+                        {
+                            OrganizationOptions[item.Key] = item;
+                        }
+                        else
+                        {
+                            OrganizationOptions.Add(item.Key, item);
+                        }
+                    }
+                }
+            }
+        }
+
+        [XmlIgnore]
+        [Browsable(false)]
+        public Dictionary<string, OrganizationOptions> OrganizationOptions { get; set; }
     }
 }
---
+++ AlbanianXrm.EarlyBound/OrganizationOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace AlbanianXrm.EarlyBound
+{
+    [Serializable]
+
+    public class OrganizationOptions
+    {
+
+        public OrganizationOptions() { }
+
+        [DisplayName("Organization")]
+        [Description("Current organization")]
+        [ReadOnly(true)]
+        public string Key { get; set; }
+
+        [Category("General")]
+        [DisplayName("Namespace")]
+        [Description("Generated namespace")]
+        public string Namespace { get; set; }
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
