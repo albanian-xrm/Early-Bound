3ded1956cd2700631a5199dd5b4713670f39964a
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2019-10-17 22:37:41 +0200

    Updated version

--- AlbanianXrm.EarlyBound.Version/Properties/AssemblyInfo.cs
+++ AlbanianXrm.EarlyBound.Version/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.2019.8.4")]
-[assembly: AssemblyFileVersion("1.2019.8.4")]
+[assembly: AssemblyVersion("1.2019.10.1")]
+[assembly: AssemblyFileVersion("1.2019.10.1")]
