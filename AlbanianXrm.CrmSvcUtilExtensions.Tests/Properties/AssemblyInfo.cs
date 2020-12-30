ee04568130b4e8600e0011ca6a4d2f4e74ff3b6a
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2020-12-30 20:45:56 +0100

    ✨ Added launchSetting to easily debug package
    - Refactored project to use Microsoft.NET.Sdk

--- AlbanianXrm.CrmSvcUtilExtensions.Tests/Properties/AssemblyInfo.cs
+++ AlbanianXrm.CrmSvcUtilExtensions.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,4 @@
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("AlbanianXrm.CrmSvcUtilExtensions.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AlbanianXrm.CrmSvcUtilExtensions.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
--- AlbanianXrm.EarlyBound/Properties/AssemblyInfo.cs
+++ AlbanianXrm.EarlyBound/Properties/AssemblyInfo.cs
@@ -1,21 +1,7 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AlbanianXrm.EarlyBound")]
-[assembly: AssemblyDescription("This plugin generates early bound entities using svcutil.exe giving the possibility to choose the wanted attributes")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Betim Beja")]
-[assembly: AssemblyProduct("AlbanianXrm.EarlyBound")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -24,5 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e047e333-0237-4fd9-b80a-1757d706630f")]
-
-[assembly: InternalsVisibleTo("AlbanianXrm.EarlyBound.Tests")]
