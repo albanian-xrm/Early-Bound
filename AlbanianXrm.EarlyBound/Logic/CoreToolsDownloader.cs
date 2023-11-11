22d38249bf8e8ac1b6fa6b12c64348eb2a5879ff
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2023-11-11 20:36:57 +0100

    removed nuget dependency

--- AlbanianXrm.EarlyBound/Logic/CoreToolsDownloader.cs
+++ AlbanianXrm.EarlyBound/Logic/CoreToolsDownloader.cs
@@ -1,16 +1,18 @@
 ﻿using AlbanianXrm.BackgroundWorker;
 using AlbanianXrm.EarlyBound.Helpers;
+using AlbanianXrm.EarlyBound.Models;
 using AlbanianXrm.EarlyBound.Properties;
 using AlbanianXrm.XrmToolBox.Shared.Extensions;
-using NuGet.Common;
-using NuGet.Packaging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NuGet.Protocol;
-using NuGet.Protocol.Core.Types;
-using NuGet.Versioning;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Packaging;
 using System.Linq;
-using System.Threading;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,24 +22,30 @@ namespace AlbanianXrm.EarlyBound.Logic
     {
         private readonly MyPluginControl myPlugin;
         private readonly AlBackgroundWorkHandler backgroundWorkHandler;
+        //private readonly HttpClient httpClient;
+        private const string coreToolsId = "Microsoft.CrmSdk.CoreTools";
+        private const string NuGetURI = "https://api.nuget.org/v3/index.json";
 
         public CoreToolsDownloader(MyPluginControl myPlugin, AlBackgroundWorkHandler backgroundWorkHandler)
         {
             this.myPlugin = myPlugin;
             this.backgroundWorkHandler = backgroundWorkHandler;
+            //var defaultWebProxy = WebRequest.DefaultWebProxy;
+            //defaultWebProxy.Credentials = CredentialCache.DefaultCredentials;
+            //// httpClient = new HttpClient(new HttpClientHandler { Proxy = defaultWebProxy, AllowAutoRedirect = true });
+            //httpClient = new HttpClient();
         }
 
         public void DownloadCoreTools(string specificVersion)
         {
-            NuGetVersion version = string.IsNullOrWhiteSpace(specificVersion) ? null : new NuGetVersion(specificVersion);
             backgroundWorkHandler.EnqueueBackgroundWork(
-                AlBackgroundWorkerFactory.NewAsyncWorker(DownloadCoreToolsAsync, version, DownloadCoreToolsEnded)
+                AlBackgroundWorkerFactory.NewAsyncWorker(DownloadCoreToolsAsync, specificVersion, DownloadCoreToolsEnded)
                                          .WithViewModel(myPlugin.pluginViewModel)
                                          .WithMessage(myPlugin, Resources.DOWNLOADING_CORE_TOOLS));
 
         }
 
-        public void DownloadCoreToolsEnded(NuGetVersion version, string value, Exception exception)
+        public void DownloadCoreToolsEnded(string version, string value, Exception exception)
         {
             try
             {
@@ -58,65 +66,133 @@ namespace AlbanianXrm.EarlyBound.Logic
             }
         }
 
-        public async Task<string> DownloadCoreToolsAsync(NuGetVersion version)
+        public async Task<string> DownloadCoreToolsAsync(string version)
         {
             //ID of the package to be looked 
-            string coreToolsId = "Microsoft.CrmSdk.CoreTools";
-
+            var nuGetFeed = new Uri(myPlugin.options.NuGetFeed ?? NuGetURI);
+            NuGetSearch nuGetSearch = null;
             string dir = Path.GetDirectoryName(typeof(MyPluginControl).Assembly.Location).ToUpperInvariant();
             string folder = Path.GetFileNameWithoutExtension(typeof(MyPluginControl).Assembly.Location);
             dir = Path.Combine(dir, folder);
             Directory.CreateDirectory(dir);
 
             //Connect to the official package repository IPackageRepository
-            ILogger logger = NullLogger.Instance;
-            CancellationToken cancellationToken = CancellationToken.None;
-            SourceCacheContext cache = new SourceCacheContext();
 
-            SourceRepository repository = Repository.Factory.GetCoreV3(myPlugin.options.NuGetFeed);
-            PackageSearchResource packageSearch = await repository.GetResourceAsync<PackageSearchResource>();
-            FindPackageByIdResource findPackageById = await repository.GetResourceAsync<FindPackageByIdResource>();
             if (version == null)
             {
-                var metadata = (await packageSearch.SearchAsync(coreToolsId, new SearchFilter(false, SearchFilterType.IsLatestVersion), 0, 1, logger, cancellationToken)).FirstOrDefault();
-                version = (await metadata.GetVersionsAsync()).Max(v => v.Version);
+                if (nuGetFeed.IsFile)
+                {
+                    var packages = Directory.GetFiles(myPlugin.options.NuGetFeed, coreToolsId + ".*.nupkg");
+                    version = packages.Select(package => Path.GetFileName(package))
+                                      .Select((package) => package.Length >= 33 ? package.Substring(27, package.Length - 33) : "")
+                                      .Where(v => Version.TryParse(v, out _))
+                                      .OrderByDescending((v) => new Version(v))
+                                      .FirstOrDefault();
+                }
+                else
+                {
+                    nuGetSearch = await GetNuGetSearch(nuGetFeed);
+                    version = nuGetSearch.Data?.Find(package => package.Id == coreToolsId)?.Version;
+                }
             }
 
 #if DEBUG
             System.Diagnostics.Debug.WriteLine($"Version {version}");
 #endif
 
-            using (MemoryStream packageStream = new MemoryStream())
+            string localPackagePath = null;
+            if (nuGetFeed.IsFile)
             {
-                if (!await findPackageById.CopyNupkgToStreamAsync(
-                     coreToolsId,
-                     version,
-                     packageStream,
-                     cache,
-                     logger,
-                     cancellationToken))
+                var packages = Directory.GetFiles(myPlugin.options.NuGetFeed, coreToolsId + ".*.nupkg");
+                localPackagePath = packages.Select(package => Path.GetFileName(package))
+                                           .Where((package) => package.Length >= 33 ? package.Substring(27, package.Length - 33) == version : false)
+                                           .Select(package => Path.Combine(myPlugin.options.NuGetFeed, package))
+                                           .FirstOrDefault();
+            }
+            else
+            {
+                if (nuGetSearch == null)
+                {
+                    nuGetSearch = await GetNuGetSearch(nuGetFeed);
+                }
+                var onlinePackageInfoPath = nuGetSearch.Data.Find(package => package.Id == coreToolsId).Versions.Find(v => v.Version == version)?.Id;
+                string packageContent = null;
+                if (onlinePackageInfoPath == null ||
+                    (packageContent = await GetPackageContent(onlinePackageInfoPath)) == null ||
+                    (localPackagePath = await DownloadNuGetPackage(packageContent)) == null)
                 {
                     return string.Format(Resources.Culture, Resources.CORE_TOOLS_NOT_FOUND, coreToolsId, myPlugin.options.NuGetFeed);
                 }
 
-                using (PackageArchiveReader packageReader = new PackageArchiveReader(packageStream))
-                {
-                    foreach (var packageFile in await packageReader.GetFilesAsync(cancellationToken))
-                    {
-                        System.Diagnostics.Debug.WriteLine($"{packageFile} {Path.GetDirectoryName(packageFile)} {Path.GetFileName(Path.GetDirectoryName(packageFile))}");
+            }
 
-                        if (Path.GetFileName(Path.GetDirectoryName(packageFile)) == "coretools")
-                        {
-                            using (var fileStream = File.OpenWrite(Path.Combine(dir, Path.GetFileName(packageFile))))
-                            using (var stream = await packageReader.GetStreamAsync(packageFile, cancellationToken))
-                            {
-                                await stream.CopyToAsync(fileStream);
-                            }
-                        }
+            var packageInfo = new FileInfo(localPackagePath);
+            using (var package = Package.Open(packageInfo.OpenRead()))
+            {
+                foreach (var part in package.GetParts())
+                {
+                    var path = part.Uri.ToString();
+
+                    if (!path.StartsWith("/content/bin/coretools", StringComparison.InvariantCultureIgnoreCase)) continue;
+
+                    var fileName = Path.GetFileName(path);
+
+
+                    using (var fileStream = File.OpenWrite(Path.Combine(dir, fileName)))
+                    using (var stream = part.GetStream())
+                    {
+                        await stream.CopyToAsync(fileStream);
                     }
                 }
             }
             return null;
+        }
+
+        private async Task<string> DownloadNuGetPackage(string packageContent)
+        {
+            var tempPath = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "AlbanianEarlyBound"));
+            tempPath.Create();
+            var fileInfo = new FileInfo(Path.Combine(tempPath.FullName, Path.GetFileName(packageContent)));
+            var   httpClient = new HttpClient { BaseAddress = new Uri(new Uri(packageContent).GetLeftPart(UriPartial.Authority)) };
+            var stream = await httpClient.GetStreamAsync(packageContent);
+            using(var fileStream = fileInfo.OpenWrite()) {
+                await stream.CopyToAsync(fileStream);
+            }
+            return fileInfo.FullName;
+        }
+
+        public async Task<string> GetPackageContent(string package)
+        {  
+            var httpClient = new HttpClient { BaseAddress = new Uri(new Uri(package).GetLeftPart(UriPartial.Authority)) };
+            var stream = await httpClient.GetStreamAsync(package);
+            using (var reader = new JsonTextReader(new StreamReader(stream)))
+            {
+                return (await JObject.LoadAsync(reader)).ToObject<NuGetPackage>().PackageContent;
+            }
+        }
+
+        public async Task<NuGetSearch> GetNuGetSearch(Uri nuGetFeed)
+        {           
+            var searchEndpoint = new Uri( await GetSearchEndpoint(nuGetFeed) + $"?q={coreToolsId}");
+            var httpClient = new HttpClient { BaseAddress = new Uri(searchEndpoint.GetLeftPart(UriPartial.Authority)) };
+            var stream = await httpClient.GetStreamAsync(searchEndpoint);
+            using (var reader = new JsonTextReader(new StreamReader(stream)))
+            {
+                return (await JObject.LoadAsync(reader)).ToObject<NuGetSearch>();
+            }
+        }
+
+        public async Task<string> GetSearchEndpoint(Uri nuGetFeed)
+        {
+            var httpClient = new HttpClient { BaseAddress = new Uri(nuGetFeed.GetLeftPart(UriPartial.Authority)) };
+            var stream = await httpClient.GetStreamAsync(nuGetFeed);
+
+            NuGetApi nuGetApi = null;
+            using (var reader = new JsonTextReader(new StreamReader(stream)))
+            {
+                nuGetApi = (await JObject.LoadAsync(reader)).ToObject<NuGetApi>();
+            }
+            return nuGetApi.Resources?.Find(resource => resource.type == "SearchQueryService")?.id;
         }
     }
 }
---
+++ AlbanianXrm.EarlyBound/Models/NuGet.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace AlbanianXrm.EarlyBound.Models
+{
+    public class NuGetApi
+    {
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("resources")]
+        public List<NuGetResource> Resources { get; set; }
+    }
+
+    public class NuGetResource
+    {
+        [JsonProperty("@id")]
+        public string @id { get; set; }
+
+        [JsonProperty("@type")]
+        public string @type { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+    }
+
+    public class NuGetSearch
+    {
+        [JsonProperty("data")]
+        public List<NuGetSearchData> Data { get; set; }
+    }
+
+    public class NuGetSearchData
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("versions")]
+        public List<NuGetVersion> Versions { get; set; }
+    }
+
+    public class NuGetVersion
+    {
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("@id")]
+        public string @Id { get; set; }
+    }
+
+    public class NuGetPackage
+    {
+        [JsonProperty("packageContent")]
+        public string PackageContent { get; set;}
+    }
+}
